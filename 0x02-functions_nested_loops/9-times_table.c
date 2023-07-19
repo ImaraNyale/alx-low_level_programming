326b9b25accafc238ccf172274bbda3c99ce3f0c
ImaraNyale <nyaleimara28@gmail.com>
2023-07-19 22:57:02 +0000

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,10 +15,10 @@ void times_table(void)
 	{
         	b = 0;
         	while (b <= 9)
-	        {
+		{
 			rep = a * b;
-	                if (b == 0)
-	                {
+			if (b == 0)
+			{
 				_putchar('0' + rep);
 			}
 			else if (rep < 10)
@@ -31,7 +31,7 @@ void times_table(void)
 				_putchar('0' + rep / 10);
 				_putchar('0' + rep % 10);
 			}
-			
+
 			if (b < 9)
 			{
 				_putchar(',');
