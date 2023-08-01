bda26d33a9af2aaf725e3489e8e0156d14307a4b
ImaraNyale <nyaleimara28@gmail.com>
2023-08-01 15:48:53 -0700

    largest nu

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,9 +14,9 @@ int largest_number(int a, int b, int c)
 
 	if (a >=  b && b >=  c)
 	{
-	largest = a;
+		largest = a;
 	}
-	else if (b > a && >=  c)
+	else if (b >=  a && a >=  c)
 	{
 		largest = b;
 	}
