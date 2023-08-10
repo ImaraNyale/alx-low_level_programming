d95cc61b42a3f69644aad086d031ba137e6a07bf
ImaraNyale <nyaleimara28@gmail.com>
2023-08-10 15:29:38 -0700

    static library

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ **_memset - function entry point
+ *@s: A pointer to the memory area to be filled
+ *@b: The character to fill the memory area with.
+ *@n: The number of bytes to be filled.
+ *Return: Success Always (0)
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+}
---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * *_strncat - Copies @n bytes from the memory area pointed
+ *           by @src into that pointed by @dest
+ * @dest: A pointer to the memory to copy @src into
+ * @src: The source buffer to copy characters from
+ * @n: The number of bytes to copy from @src
+ *
+ * Return: A pointer to the destination buffer @dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+
+}
