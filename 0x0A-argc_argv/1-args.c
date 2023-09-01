34f69b28d9cc063ee13a9675b800ce2967aca6d0
ImaraNyale <nyaleimara28@gmail.com>
2023-09-01 15:16:02 -0700

    argument count

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,12 +4,12 @@
  * main - prints argument count
  * @argc: argument count
  * @argv: argument vector
- * Return: Always 0 
+ * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
 	(void)*argv;
 
 	printf("%d\n", argc - 1);
-	return(0);
+	return (0);
 }
