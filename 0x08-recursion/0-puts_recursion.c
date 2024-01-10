82958bc752633f7cc7d060034e4327b5ec6eda2f
ImaraNyale <nyaleimara28@gmail.com>
2024-01-10 09:58:40 -0800

    puts with recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *
+ *_puts_recursion: function prints string
+ *@s: character to be printed
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else 
+		_putchar('\n');
+}
