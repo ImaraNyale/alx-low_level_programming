68d8b9708a9f3dda250172541238723cc3b89da0
ImaraNyale <nyaleimara28@gmail.com>
2024-01-16 01:58:17 -0800

    crate array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+/*
+ **create_array - function creates string
+ *@size: size of the string
+ *@c: char to assign
+ *Return: NULL iffail/pointer if success
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int o;
+
+	str = malloc(sizeof(char) * size);
+
+	if (size == 0 || str == 0)
+		return (NULL);
+	for (o = 0 ; o < size; o++)
+		str[o] = c;
+	return (str);
+
+}
