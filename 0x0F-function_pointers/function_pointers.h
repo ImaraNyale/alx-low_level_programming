c838f82669242a921c989aa749779b305adcd16a
ImaraNyale <nyaleimara28@gmail.com>
2024-01-24 13:20:29 -0800

    search for integer

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -6,4 +6,5 @@ void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 #include <stddef.h>
 #include <stdio.h>
+int int_index(int *array, int size, int (*cmp)(int));
 #endif
